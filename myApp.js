3aed62ca53d13ae0485b0b00b6f163380fe4dc37
Тимофей Трофимов <t.trofimov@just-work.org>
2021-08-06 08:58:56 +0300

    Create a Model

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
