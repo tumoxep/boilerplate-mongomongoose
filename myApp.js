9ca8e50027e8c39e38dc0db61127dcc62c623fe6
Тимофей Трофимов <t.trofimov@just-work.org>
2021-08-06 13:34:32 +0300

    Perform New Updates on a Document Using model.findOneAndUpdate()

--- myApp.js
+++ myApp.js
@@ -60,8 +60,9 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, function(err, person) {
+    done(err, person);
+  });
 };
 
 const removeById = (personId, done) => {
