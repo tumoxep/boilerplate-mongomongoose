c5f1af4561f885cabe9d2198a1dd2c9eefeab37a
Тимофей Трофимов <t.trofimov@just-work.org>
2021-08-06 09:29:16 +0300

    Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -27,7 +27,9 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function(err, data) {
+    done(err, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
